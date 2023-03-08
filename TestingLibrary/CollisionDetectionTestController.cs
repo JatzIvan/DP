e3308fdd6c3a380260eaf43eaa26af17b5ad3b5b
Ivan Jatz <jatzivan@gmail.com>
2023-03-09 00:58:22 +0100

    Refactored tests into separate classes

---
+++ TestingLibrary/CollisionDetectionTestController.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketLibrary.Models;
+using WebSocketLibrary;
+using CoreLibrary.RoadSectionHandling.CollisionCalculators;
+using CoreLibrary.RoadSectionHandling.Model;
+using CoreLibrary.RoadSectionHandling;
+using CoreLibrary.RoadSectionHandling.CurvatureCalculations;
+using CoreLibrary.RoadSectionHandling.RoadSimplificators;
+using CoreLibrary;
+using System.Reflection;
+using Xunit;
+
+namespace TestingLibrary
+{
+    [Collection("Sequential")]
+    public class CollisionDetectionTestController: AbstractTestController
+    {
+        
+        /**
+         * Data setup
+         */
+        public static class VehicleWrapperData
+        {
+            private static readonly List<object[]> _data = new List<object[]>();
+
+            static VehicleWrapperData()
+            {
+                foreach (string file in Directory.EnumerateFiles(@"Resources/VehicleScenarios", "*.json"))
+                {
+                    string jsonString = File.ReadAllText(file);
+                    CarUpdateInfo roadRaw = JsonConvert.DeserializeObject<CarUpdateInfo>(jsonString)!;
+                    _data.Add(new object[] { new VehicleObserverWrapper(roadRaw, 0), Convert.ToBoolean(file.Split("_")[1].Replace(".json", "")) });
+
+                }
+            }
+
+            public static IEnumerable<object[]> TestData
+            {
+                get { return _data; }
+            }
+        }
+
+        /**
+         * Setup for Test collision detector
+         */
+        class TestCollisionDetector : AbstractCollisionDetector
+        {
+
+            private RoadDataHandler dataStorage;
+
+            private bool collisionOccured;
+
+            public TestCollisionDetector(RoadDataHandler roadHandler, bool collisionOccured) : base(roadHandler.GetParsedRoadData())
+            {
+                roadHandler.GatherCurvaturesBetweenVehicles();
+                this.dataStorage = roadHandler;
+                this.collisionOccured = collisionOccured;
+            }
+
+            public override void PerformActions(VehicleObserverWrapper data)
+            {
+                List<VehicleData> vehicles = data.Data.Vehicles;
+                //Console.WriteLine("Num of vehicles recieved " + vehicles.Count);
+                if (vehicles.Count >= 2)
+                {
+                    IEnumerable<IEnumerable<VehicleData>> pairsToCalc = CreateVehiclePairs(vehicles);
+
+                    // Try threading or something, right now I need to ensure that this concept can work
+                    foreach (var pair in pairsToCalc)
+                    {
+                        ICollisionCalculatorImplementation calcMethod = ResolveCollisionCalculatorBasedOnCurvature(pair.ElementAt(0), pair.ElementAt(1));
+                        if (calcMethod != null)
+                        {
+                            AbstractRoadModel collisionPoint = calcMethod.PerformCollisionCalculations(pair.ElementAt(0), pair.ElementAt(1), currectRoadModel);
+
+                            Xunit.Assert.Equal(collisionOccured, calcMethod.CollisionOccured());
+                        }
+                    }
+                }
+            }
+
+        }
+
+        // ------------------------------------------------------------ //
+
+        [Theory]
+        [MemberData(nameof(VehicleWrapperData.TestData), MemberType = typeof(VehicleWrapperData))]
+        public void TestCollisionSituations(VehicleObserverWrapper wrapper, bool collision)
+        {
+
+            ApplicationConfigurationHandler.CurvetureCalcMethod = typeof(CircumcircleRoadCircleCurvesResolver).Name;
+            ApplicationConfigurationHandler.SimplificationMethod = typeof(DouglasPeuckerRoadSectionSimplification).Name;
+            ApplicationConfigurationHandler.DPTolerance = 0.5f;
+            ApplicationConfigurationHandler.CurvatureTreshold = 0.006f;
+            ApplicationConfigurationHandler.CarDistanceSkipTreshold = 500;
+
+            RoadDataHandler handler = new RoadDataHandler("503", "503");
+            handler.roadDataFether = roadDataFetcher.Object;
+
+            new TestCollisionDetector(handler, collision).PerformActions(wrapper);
+        }
+
+
+
+    }
+}
--- TestingLibrary/TestController.cs
+++ TestingLibrary/TestController.cs
@@ -13,6 +13,7 @@ using Newtonsoft.Json;
 
 namespace TestingLibrary
 {
+    [Collection("Sequential")]
     public class TestController : AbstractTestController
     {
 
@@ -24,6 +25,8 @@ namespace TestingLibrary
         [Fact]
         public void TestRoadHandlerWithDefaultConfiguration()
         {
+            ApplicationConfigurationHandler.DPTolerance = 0;
+            ApplicationConfigurationHandler.SimplificationMethod = typeof(DouglasPeuckerRoadSectionSimplification).Name;
             RoadDataHandler handler = new RoadDataHandler("503", "503");
             handler.roadDataFether = roadDataFetcher.Object;
 
@@ -40,7 +43,7 @@ namespace TestingLibrary
         [InlineData(1, 415)]
         public void TestRoadSimplificationWithDouglasPeucker(float simplificationTolerance, float expectedNumberOfPoints)
         {
-
+            ApplicationConfigurationHandler.LoadConfiguration();
             ApplicationConfigurationHandler.SimplificationMethod = typeof(DouglasPeuckerRoadSectionSimplification).Name;
             ApplicationConfigurationHandler.DPTolerance = simplificationTolerance;
 
@@ -60,7 +63,7 @@ namespace TestingLibrary
         [InlineData(1, 5, 367)]
         public void TestRoadSimplificationWithLang(float simplificationTolerance, int regionSize, float expectedNumberOfPoints)
         {
-
+            ApplicationConfigurationHandler.LoadConfiguration();
             ApplicationConfigurationHandler.SimplificationMethod = typeof(LangRoadSectionSimplification).Name;
             ApplicationConfigurationHandler.DPTolerance = simplificationTolerance;
             ApplicationConfigurationHandler.LangRegionSize = regionSize;
@@ -83,7 +86,9 @@ namespace TestingLibrary
         [InlineData(0.01, 344)]
         public void TestCircleCurvatureResolver(float curveTolerance, int numberOfDangerousRegions)
         {
-
+            ApplicationConfigurationHandler.LoadConfiguration();
+            ApplicationConfigurationHandler.DPTolerance = 0;
+            ApplicationConfigurationHandler.SimplificationMethod = typeof(DouglasPeuckerRoadSectionSimplification).Name;
             ApplicationConfigurationHandler.CurvetureCalcMethod = typeof(CircumcircleRoadCircleCurvesResolver).Name;
 
             RoadDataHandler handler = new RoadDataHandler("503", "503");
@@ -113,80 +118,5 @@ namespace TestingLibrary
 
         }
 
-        [Theory]
-        [MemberData(nameof(VehicleWrapperData.TestData), MemberType = typeof(VehicleWrapperData))]
-        public void TestCollisionSituations(VehicleObserverWrapper wrapper, bool collision)
-        {
-
-            ApplicationConfigurationHandler.CurvetureCalcMethod = typeof(CircumcircleRoadCircleCurvesResolver).Name;
-            ApplicationConfigurationHandler.SimplificationMethod = typeof(DouglasPeuckerRoadSectionSimplification).Name;
-            ApplicationConfigurationHandler.DPTolerance = 0.5f;
-            ApplicationConfigurationHandler.CurvatureTreshold = 0.006f;
-            ApplicationConfigurationHandler.CarDistanceSkipTreshold = 500;
-
-            RoadDataHandler handler = new RoadDataHandler("503", "503");
-            handler.roadDataFether = roadDataFetcher.Object;
-
-            new TestCollisionDetector(handler, collision).PerformActions(wrapper);
-        }
-
-        class TestCollisionDetector : AbstractCollisionDetector
-        {
-
-            private RoadDataHandler dataStorage;
-
-            private bool collisionOccured;
-
-            public TestCollisionDetector(RoadDataHandler roadHandler, bool collisionOccured) : base(roadHandler.GetParsedRoadData())
-            {
-                roadHandler.GatherCurvaturesBetweenVehicles();
-                this.dataStorage = roadHandler;
-                this.collisionOccured = collisionOccured;
-            }
-
-            public override void PerformActions(VehicleObserverWrapper data)
-            {
-                List<VehicleData> vehicles = data.Data.Vehicles;
-                //Console.WriteLine("Num of vehicles recieved " + vehicles.Count);
-                if (vehicles.Count >= 2)
-                {
-                    IEnumerable<IEnumerable<VehicleData>> pairsToCalc = CreateVehiclePairs(vehicles);
-
-                    // Try threading or something, right now I need to ensure that this concept can work
-                    foreach (var pair in pairsToCalc)
-                    {
-                        ICollisionCalculatorImplementation calcMethod = ResolveCollisionCalculatorBasedOnCurvature(pair.ElementAt(0), pair.ElementAt(1));
-                        if (calcMethod != null)
-                        {
-                            AbstractRoadModel collisionPoint = calcMethod.PerformCollisionCalculations(pair.ElementAt(0), pair.ElementAt(1), currectRoadModel);
-
-                            Xunit.Assert.Equal(collisionOccured, calcMethod.CollisionOccured());
-                        }
-                    }
-                }
-            }
-
-        }
-
-
-        public static class VehicleWrapperData
-        {
-            private static readonly List<object[]> _data = new List<object[]>();
-
-            static VehicleWrapperData(){
-                foreach (string file in Directory.EnumerateFiles(@"Resources/VehicleScenarios", "*.json"))
-                {
-                    string jsonString = File.ReadAllText(file);
-                    CarUpdateInfo roadRaw = JsonConvert.DeserializeObject<CarUpdateInfo>(jsonString)!;
-                    _data.Add(new object[] { new VehicleObserverWrapper(roadRaw, 0), Convert.ToBoolean(file.Split("_")[1].Replace(".json", "")) });
-
-                }
-            }
-
-            public static IEnumerable<object[]> TestData
-            {
-                get { return _data; }
-            }
-        }
     }
 }
